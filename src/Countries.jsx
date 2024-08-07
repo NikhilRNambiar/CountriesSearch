843d73f8778b4711af2239ec0407ac85d95614bd
NikhilRNambiar <nikhilrnambiar007@gmail.com>
2024-08-07 23:58:37 +0530

    first commit

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import "./Countries.css"
 
+function Card({img,imgAlt,name}){
+    return <div className='countryCard'>
+        <img src={img} alt={imgAlt} style={{width:"100px", height:"100px"}}/>
+        <h2>{name}</h2>
+    </div>
+}
 const url="https://restcountries.com/v3.1/all";
 function Countries() {
 
     const[country,setCountry]=useState([]);
-    const[searchTerm,setSearchTerm]=useState('');
+    const[searchTerm,setSearchTerm]=useState();
 
     useEffect(()=>{
         const fetchData=async()=>{
@@ -24,25 +30,25 @@ function Countries() {
     },[])
 
     const handleSearch=(event)=>{
-        setSearchTerm(event.target.value)
+        setSearchTerm(event.target.value.toLowerCase())
     }
 
     const filteredData=country.filter((countries) =>
-        countries.name.common.includes(searchTerm));
+        countries.name.common.toLowerCase().includes(searchTerm));
   return (
     <div>
         <div style={{display:'flex',justifyContent:'center', alignItems:'center',backgroundColor:'#f8f8ff'}}>
-            <input type='text' onChange={handleSearch} value={searchTerm} className='searchBar' placeholder='Search for Countries'/>
+            <input type='text' onChange={handleSearch} value={searchTerm} className='searchBar'/>
         </div>
         <div style={{display:"flex",
             flexWrap:"wrap"
          }}>
             {(searchTerm?filteredData:country).map((countries)=>(
-                // <Card key={countries.name.common} img={countries.flags.png} imgAlt={countries.name.common} name={countries.name.common}/>
-                <div className='countryCard' key={countries.name.common}>
-                    <img src={countries.flags.png} alt={countries.name.common} style={{width:"100px", height:"100px"}}/>
-                    <h2>{countries.name.common}</h2>
-                </div>
+                <Card key={countries.name.common} img={countries.flags.png} imgAlt={countries.name.common} name={countries.name.common}/>
+                // <div className='countryCard' >
+                //     <img src={countries.flags.png} alt={countries.name.common} style={{width:"100px", height:"100px"}}/>
+                //     <h3>{countries.name.common}</h3>
+                // </div>
                 
                 ))}
         </div>
